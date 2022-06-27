979f99d173891334357d0f4d73407fc38687f4e5
PulkitGupta09 <89147343+PulkitGupta09@users.noreply.github.com>
2022-06-27 08:00:55 +0530

    video 46 completed express-validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator'); 
 
 
 //create a User using: POST "/api/auth/". Doesn't require auth
-router.get('/',(req,res)=>{
-    console.log(req .body);
-    const user = User(req.body);
-    user.save()
-    res.json(req.body);
-})
+router.post('/',[
+  body('email',"Enter a valid Email").isEmail(),
+  body('name',"Enter a valid name").isLength({ min: 3 }),
+  body('password',"password must atleast 5 characters").isLength({ min: 5 }),
+],(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err=>{console.log(err)
+    res.json({error: 'please enter a unique value for email'})})
+  })
 
 module.exports = router; 
