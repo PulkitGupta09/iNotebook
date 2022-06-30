0b242fd63ec6aa587e2c54646c55532b7a72d70b
PulkitGupta09 <89147343+PulkitGupta09@users.noreply.github.com>
2022-07-01 01:15:04 +0530

    video 63 completed

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,18 +1,22 @@
  import React,{useContext}  from 'react'
 import noteContext from "../context/notes/NoteContext"
+import AddNote from './AddNote';
 import NoteItem from "./Noteitem"
 
 
 function Notes() {
     const context = useContext(noteContext);
-    const {notes,setNotes} = context;
+    const {notes,addNote} = context;
   return (
+    <>
+    <AddNote/>
     <div className="row my-3">
     <h1>Your Notes</h1>
     {notes.map((note)=>{
-     return <NoteItem note = {note}/>;
+     return <NoteItem key = {note._id} note = {note}/>;
     })}
 </div>  
+</>
   )
 }
 
--- src/context/notes/Notestate.js
+++ src/context/notes/Notestate.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const NoteState = (props)=>{
     const notesInitial = [
         {
-                "_id": "62bb29b3384b5efe35f9ce11",
+                "_id": "62bb29b3384b5efea35f9ce11",
                 "user": "62ba90ae3a3767a7d5df718b",
                 "title": "PUlkit Gupta",
                 "description": "Hello i am pulkit gupta",
@@ -12,7 +12,7 @@ const NoteState = (props)=>{
                 "__v": 0
               },
               {
-                "_id": "62bda8cc2eadd317a23c7f7a",
+                "_id": "62bda8cc2eadd3v17a23c7f7a",
                 "user": "62ba90ae3a3767a7d5df718b",
                 "title": "PUlkit Gupta",
                 "description": "Hello i am pulkit gupta",
@@ -21,7 +21,7 @@ const NoteState = (props)=>{
                 "__v": 0
               },
               {
-                "_id": "62bb29b3384b5efe35f9ce11",
+                "_id": "62bb29b338h4b5efe35f9ce11",
                 "user": "62ba90ae3a3767a7d5df718b",
                 "title": "PUlkit Gupta",
                 "description": "Hello i am pulkit gupta",
@@ -30,7 +30,7 @@ const NoteState = (props)=>{
                 "__v": 0
               },
               {
-                "_id": "62bda8cc2eadd317a23c7f7a",
+                "_id": "62bda8cc2eaddw317a23c7f7a",
                 "user": "62ba90ae3a3767a7d5df718b",
                 "title": "PUlkit Gupta",
                 "description": "Hello i am pulkit gupta",
@@ -39,7 +39,7 @@ const NoteState = (props)=>{
                 "__v": 0
               },
               {
-                "_id": "62bb29b3384b5efe35f9ce11",
+                "_id": "62bb29b3384b5efe35yf9ce11",
                 "user": "62ba90ae3a3767a7d5df718b",
                 "title": "PUlkit Gupta",
                 "description": "Hello i am pulkit gupta",
@@ -48,7 +48,7 @@ const NoteState = (props)=>{
                 "__v": 0
               },
               {
-                "_id": "62bda8cc2eadd317a23c7f7a",
+                "_id": "62bda8cc2eadd317a2a3c7f7a",
                 "user": "62ba90ae3a3767a7d5df718b",
                 "title": "PUlkit Gupta",
                 "description": "Hello i am pulkit gupta",
@@ -58,11 +58,33 @@ const NoteState = (props)=>{
               }
     ]
     const [notes,setNotes] = useState(notesInitial)
+
+    // Add a Note
+    const addNote = (title,description,tag)=>{
+        const note = {
+            "_id": "62bda8cc2eadd31b7a23c7f7a",
+            "user": "62ba90ae3a3767a7d5df718b",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "date": "2022-06-30T13:44:44.787Z",
+            "__v": 0
+        }
+        setNotes(notes.concat(note))
+    }
+    // Delete a Note
+    const deleteNote = ()=>{
+        
+    }
+    // Edit a Note
+    const editNote = ()=>{
+        
+    }
 return(
-<NoteContext.Provider value = {{notes,setNotes}}>
+<NoteContext.Provider value = {{notes,addNote,deleteNote,editNote}}>
         {props.children};
     </NoteContext.Provider>
 )
 }
-
+ 
 export default NoteState;
